3dff5a8c0e2d4ea555b69650342da46a0ffcefa3
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-06-11 15:59:20 +0200

    Add logging

--- src/core/schedules/schedule.controller.ts
+++ src/core/schedules/schedule.controller.ts
@@ -16,31 +16,39 @@ export class ScheduleController {
 
   @MessagePattern({ cmd: 'create_schedule' })
   async create(@Payload() data: CreateScheduleRequestDto) {
+    this.logger.log('Received create schedule record');
     return this.scheduleService.create(data);
   }
 
   @MessagePattern({ cmd: 'find_all_schedules' })
   findAll(query: PaginationQueryDto) {
+    this.logger.log('Received find all schedules request');
     return this.scheduleService.findAll(query);
   }
 
   @MessagePattern({ cmd: 'find_one_schedule' })
   findOne(@Payload() id: number) {
+    this.logger.log('Received find one schedule request');
     return this.scheduleService.findOne(id);
   }
 
   @MessagePattern({ cmd: 'find_schedules_by_program' })
   findAllByProgram(@Payload() query: SearchPaginationQueryDto) {
+    this.logger.log('Received find schedules by program request');
     return this.scheduleService.findAllByProgram(query);
   }
 
   @MessagePattern({ cmd: 'find_schedules_by_program_between_dates' })
   findAllBetweenDates(@Payload() query: TemporalSearchQueryDto) {
+    this.logger.log('Received find schedules by program between dates request');
     return this.scheduleService.findAllByProgramBetweenDates(query);
   }
 
   @MessagePattern({ cmd: 'find_schedules_by_instructor_between_dates' })
   findAllByInstructorBetweenDates(@Payload() query: TemporalSearchQueryDto) {
+    this.logger.log(
+      'Received find schedules by instructor between dates request',
+    );
     return this.scheduleService.findAllByInstructorBetweenDates(query);
   }
 
@@ -52,11 +60,13 @@ export class ScheduleController {
       updateData: UpdateScheduleRequestDto;
     },
   ) {
+    this.logger.log('Received update schedule request');
     return this.scheduleService.update(payload.id, payload.updateData);
   }
 
   @MessagePattern({ cmd: 'remove_schedule' })
   remove(@Payload() id: number) {
+    this.logger.log('Received remove schedule request');
     return this.scheduleService.remove(id);
   }
 }
